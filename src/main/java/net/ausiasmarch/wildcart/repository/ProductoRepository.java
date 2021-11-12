d0b677426244c68dae0ba8a5a9c509dd30f1f428
Mark Coronado Cabanes <cabanesdev@gmail.com>
2021-11-12 12:05:18 +0100

    create producto repository

---
+++ src/main/java/net/ausiasmarch/wildcart/repository/ProductoRepository.java
@@ -0,0 +1,8 @@
+package net.ausiasmarch.wildcart.repository;
+
+import net.ausiasmarch.wildcart.entity.ProductoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
+
+}
