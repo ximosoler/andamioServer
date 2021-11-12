c0e9fe8c6c1463272f2ff66b0cafe374e5a22b2d
Pewinfire <s.dung@hotmail.es>
2021-11-12 11:18:01 +0100

    Creacion repositorio factura

---
+++ src/main/java/net/ausiasmarch/wildcart/repository/FacturaRepository.java
@@ -0,0 +1,8 @@
+package net.ausiasmarch.wildcart.repository;
+
+import net.ausiasmarch.wildcart.entity.FacturaEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacturaRepository extends JpaRepository<FacturaEntity, Long> {
+
+}
