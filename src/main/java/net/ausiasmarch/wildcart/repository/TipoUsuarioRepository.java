d9d8116d53972e412ffc28ea52ad88cc7474db28
a023864594r <a023864594r@S09-PC16>
2021-11-12 11:16:52 +0100

    Repositorio Tipo Usuario

---
+++ src/main/java/net/ausiasmarch/wildcart/repository/TipoUsuarioRepository.java
@@ -0,0 +1,9 @@
+package net.ausiasmarch.wildcart.repository;
+
+import net.ausiasmarch.wildcart.entity.TipoUsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoUsuarioRepository extends JpaRepository<TipoUsuarioEntity, Long> {
+    
+    
+}
