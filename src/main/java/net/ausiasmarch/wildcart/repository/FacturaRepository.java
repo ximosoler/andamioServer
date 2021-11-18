f0d05dc6ead3954afe45180a29dceeda656a26d4
a023862896m <a023862896m@S09-PC09>
2021-11-18 10:37:18 +0100

    Creacion de api Factura

--- src/main/java/net/ausiasmarch/wildcart/repository/FacturaRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/FacturaRepository.java
@@ -1,8 +1,18 @@
 package net.ausiasmarch.wildcart.repository;
 
 import net.ausiasmarch.wildcart.entity.FacturaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface FacturaRepository extends JpaRepository<FacturaEntity, Long> {
+
+ @Query(value = "SELECT * FROM factura f WHERE f.id_usuario = :id_usuario", nativeQuery = true)
+    Page<FacturaEntity> findByFacturaXUsuario(Long id_usuario, Pageable pageable);
+    
+@Query(value = "SELECT COUNT(*) FROM factura where id_usuario = :id_usuario ", nativeQuery = true)
+    Long findByFacturaIdUsuarioCount(Long id_usuario);
+
 
 }
