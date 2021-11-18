810f4d4881c0b2635ac8ba77ca5d2d1dc027e2ab
josepg90 <primogil.jose@gmail.com>
2021-11-18 09:44:41 +0100

    "CompraController creada"

--- src/main/java/net/ausiasmarch/wildcart/repository/CompraRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/CompraRepository.java
@@ -1,8 +1,19 @@
 package net.ausiasmarch.wildcart.repository;
 
 import net.ausiasmarch.wildcart.entity.CompraEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CompraRepository extends JpaRepository<CompraEntity, Long> {
 
+    @Query(value = "SELECT * FROM compra where id_factura IN (SELECT id FROM factura WHERE id_usuario = :id_usuario)", nativeQuery = true)
+    Page<CompraEntity> findByCompraIdUsuarioPage(Long id_usuario, Pageable pageable);
+
+    @Query(value = "SELECT COUNT(*) FROM compra where id_factura IN (SELECT id FROM factura WHERE id_usuario = :id_usuario)", nativeQuery = true)
+    Long findByCompraIdUsuarioCount(Long id_usuario);
+    
+    @Query(value = "SELECT * FROM compra where id_factura IN (SELECT id FROM factura WHERE id_usuario = :id_usuario) AND id = :id_compra", nativeQuery = true)
+    CompraEntity findByCompraIdUsuarioView(Long id_usuario, Long id_compra);
 }
