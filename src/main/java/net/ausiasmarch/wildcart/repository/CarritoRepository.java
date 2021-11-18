092bb1f275c4f8e107a69666e69fe0f071551e83
a023317231s <a023317231s@S09-PC22>
2021-11-18 09:36:06 +0100

    Funcionalidad del carrito añadida

--- src/main/java/net/ausiasmarch/wildcart/repository/CarritoRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/CarritoRepository.java
@@ -1,8 +1,22 @@
 package net.ausiasmarch.wildcart.repository;
 
+
+import javax.transaction.Transactional;
 import net.ausiasmarch.wildcart.entity.CarritoEntity;
+import net.ausiasmarch.wildcart.entity.ProductoEntity;
+import net.ausiasmarch.wildcart.entity.UsuarioEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarritoRepository extends JpaRepository<CarritoEntity, Long> {
 
+    Page<CarritoEntity> findAllByUsuario(UsuarioEntity usuario, Pageable oPageable);
+    Page<CarritoEntity> findAllByIdIgnoreCaseContainingOrCantidadIgnoreCaseContainingOrPrecioIgnoreCaseContainingOrProductoIgnoreCaseContainingOrUsuarioIgnoreCaseContaining(String id, String cantidad, String precio, String producto, String usuario, Pageable oPageable);
+
+    CarritoEntity findByUsuarioAndProducto(UsuarioEntity usuario, ProductoEntity producto);
+    @Transactional
+    int deleteAllByUsuario(UsuarioEntity usuario);
+    @Transactional
+    int deleteByUsuarioAndProducto(UsuarioEntity usuario, ProductoEntity producto);
 }
