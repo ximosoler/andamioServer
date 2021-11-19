ca6ffbeeab86d5aa7381f5252d652c72b4d366fa
Rafael Aznar <rafaaznar@gmail.com>
2021-11-19 12:34:00 +0100

    Update CompraController.java

--- src/main/java/net/ausiasmarch/wildcart/api/CompraController.java
+++ src/main/java/net/ausiasmarch/wildcart/api/CompraController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/compra")
 public class CompraController {
 
     @Autowired
