b4c1f56c2187c7d84272969fb201b4376b615e85
a023864594r <a023864594r@S09-PC16>
2021-11-25 09:42:11 +0100

    arreglado filtro

--- src/main/java/net/ausiasmarch/wildcart/repository/ProductoRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/ProductoRepository.java
@@ -1,8 +1,9 @@
 package net.ausiasmarch.wildcart.repository;
 
 import net.ausiasmarch.wildcart.entity.ProductoEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
-
-}
+Page<ProductoEntity> findByNombreIgnoreCaseContainingOrCodigoIgnoreCaseContaining(String nombre,String codigo, Pageable oPageable);}
