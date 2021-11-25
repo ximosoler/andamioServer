8827d714d4e9c37ab4b6b86636b5c2074ef19507
Josevi97 <josevi00@hotmail.com>
2021-11-25 11:29:28 +0100

    added user filter

--- src/main/java/net/ausiasmarch/wildcart/repository/UsuarioRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/UsuarioRepository.java
@@ -1,9 +1,16 @@
 package net.ausiasmarch.wildcart.repository;
 
 import net.ausiasmarch.wildcart.entity.UsuarioEntity;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
     UsuarioEntity findByLoginAndPassword(String login, String password);
+
+    Page<UsuarioEntity> findByTipousuarioIdAndNombreIgnoreCaseContaining(Long tipo, String nombre, Pageable oPageable);
+
+    Page<UsuarioEntity> findByNombreIgnoreCaseContaining(String nombre, Pageable oPageable);
 }
