f7ed55401ee9a6e0721423d06b7fee355f5068da
MarcosSesa <marcoxxtee@gmail.com>
2021-11-25 13:23:45 +0100

    Unificacion filtro y filtro producto por tipo y nombreo codigo

--- src/main/java/net/ausiasmarch/wildcart/repository/ProductoRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/ProductoRepository.java
@@ -4,10 +4,14 @@ import net.ausiasmarch.wildcart.entity.ProductoEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductoRepository extends JpaRepository<ProductoEntity, Long> {
 
     Page<ProductoEntity> findByNombreIgnoreCaseContainingOrCodigoIgnoreCaseContaining(String nombre, String codigo, Pageable oPageable);
 
-    public Page<ProductoEntity> findByTipoproductoIdAndNombreIgnoreCaseContainingOrCodigoIgnoreCaseContaining(Long filtertype, String filter, String filter0, Pageable oPageable);
+    @Query(
+            value = "SELECT * FROM producto WHERE id_tipoproducto = ?1 AND (nombre LIKE  %?2% OR codigo LIKE %?3%)",
+            nativeQuery = true)
+    Page<ProductoEntity> findByTipoproductoIdAndNombreOrCodigo(long IdTipoproducto, String nombre, String codigo, Pageable oPageable);
 }
