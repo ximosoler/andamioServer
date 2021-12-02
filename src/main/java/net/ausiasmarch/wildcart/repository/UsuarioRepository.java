e1b84209aa83e0c44247abc0859c713f172cd0fc
RAFAEL AZNAR <rafaaznar@gmail.com>
2021-12-02 11:35:18 +0100

    arreglos usuario

--- src/main/java/net/ausiasmarch/wildcart/repository/UsuarioRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/UsuarioRepository.java
@@ -5,6 +5,7 @@ import net.ausiasmarch.wildcart.entity.UsuarioEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
 
