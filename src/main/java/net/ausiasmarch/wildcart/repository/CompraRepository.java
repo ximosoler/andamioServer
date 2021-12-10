c9effdc2c1ed6d7d99815bfc694fddb936f781e0
perevill <pevipey@gmail.com>
2021-12-10 12:12:43 +0100

    update getpage compracontroller y repository

--- src/main/java/net/ausiasmarch/wildcart/repository/CompraRepository.java
+++ src/main/java/net/ausiasmarch/wildcart/repository/CompraRepository.java
@@ -9,16 +9,39 @@ import org.springframework.data.jpa.repository.Query;
 public interface CompraRepository extends JpaRepository<CompraEntity, Long> {
 
     @Query(
+            value = "SELECT * FROM compra where id_factura IN (SELECT id FROM factura WHERE id_usuario = ?1)",
+            nativeQuery = true)
+    Page<CompraEntity> findAllUsuario(long idusuario, Pageable oPageable);
+
+    @Query(
             value = "SELECT * FROM compra WHERE id_factura = ?1 AND (cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6%)",
             nativeQuery = true)
     Page<CompraEntity> findByFacturaIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProducto(long IdFactura, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Pageable oPageable);
 
+    @Query(
+            value = "SELECT * FROM compra WHERE id_factura = ?1 AND (cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6%) AND id_factura IN (SELECT id FROM factura WHERE id_usuario = ?7)",
+            nativeQuery = true)
+    Page<CompraEntity> findByFacturaIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProductoUsuario(long IdFactura, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Long idusuario, Pageable oPageable);
+
     Page<CompraEntity> findByFacturaId(Long factura, Pageable oPageable);
+
+    @Query(value = "SELECT * FROM compra where id_factura IN (SELECT id FROM factura WHERE id_usuario = ?1) AND id_factura = ?2", nativeQuery = true)
+    Page<CompraEntity> findByFacturaIdUsuario(Long id_usuario, Long id_factura, Pageable oPageable);
 
     @Query(
             value = "SELECT * FROM compra WHERE id_producto = ?1 AND (cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6%)",
             nativeQuery = true)
     Page<CompraEntity> findByProductoIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProducto(long IdProducto, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Pageable oPageable);
+
+    @Query(
+            value = "SELECT * FROM compra WHERE id_producto = ?1 AND (cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6%) AND id_factura IN (SELECT id FROM factura WHERE id_usuario = ?7)",
+            nativeQuery = true)
+    Page<CompraEntity> findByProductoIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProductoUsuario(long IdProducto, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Long idusuario, Pageable oPageable);
+
+    @Query(
+            value = "SELECT * FROM compra WHERE id_producto = ?1 AND id_factura IN (SELECT id FROM factura WHERE id_usuario = ?2)",
+            nativeQuery = true)
+    Page<CompraEntity> findByProductoIdUsuario(Long producto, Long idusuario, Pageable oPageable);
 
     Page<CompraEntity> findByProductoId(Long producto, Pageable oPageable);
 
@@ -26,6 +49,11 @@ public interface CompraRepository extends JpaRepository<CompraEntity, Long> {
             value = "SELECT * FROM compra WHERE id LIKE  %?1% OR cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6%",
             nativeQuery = true)
     Page<CompraEntity> findByIdContain(String IdCompra, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Pageable oPageable);
+
+    @Query(
+            value = "SELECT * FROM compra WHERE id LIKE  %?1% OR cantidad LIKE  %?2% OR precio LIKE %?3% OR fecha LIKE %?4% OR descuento_usuario LIKE %?5% OR descuento_producto LIKE %?6% AND where id_factura IN (SELECT id FROM factura WHERE id_usuario = ?7) ",
+            nativeQuery = true)
+    Page<CompraEntity> findByIdContainUsuario(String IdCompra, String cantidad, String precio, String fecha, String descuentoUsuario, String descuentoProducto, Long idusuario, Pageable oPageable);
 
     Page<CompraEntity> findByCantidadIgnoreCaseContainingOrPrecioIgnoreCaseContainingOrFechaIgnoreCaseContaining(String cantidad, String precio, String fecha, Pageable oPageable);
 
