bcb5294a8857f1a3770de204ea5d28aec915f3f9
rafaelaznar <rafaaznar@gmail.com>
2022-06-03 11:13:00 +0200

    mejora excepciones

--- src/main/java/net/ausiasmarch/wildcart/Exception/AppExceptionHandler.java
+++ src/main/java/net/ausiasmarch/wildcart/Exception/AppExceptionHandler.java
@@ -19,34 +19,34 @@ public class AppExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<?> UnauthorizedException(UnauthorizedException ex, WebRequest request) {
         ErrorResponseBean errorDetails
-                = new ErrorResponseBean(new Date(), HttpStatus.UNAUTHORIZED.toString(), ex.getMessage(), request.getDescription(false));
+                = new ErrorResponseBean(new Date(), HttpStatus.UNAUTHORIZED.name(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> ResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         ErrorResponseBean errorDetails
-                = new ErrorResponseBean(new Date(), HttpStatus.NOT_FOUND.toString(), ex.getMessage(), request.getDescription(false));
+                = new ErrorResponseBean(new Date(), HttpStatus.NOT_FOUND.name(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler(ResourceNotModifiedException.class)
     public ResponseEntity<?> ResourceNotModifiedException(ResourceNotModifiedException ex, WebRequest request) {
         ErrorResponseBean errorDetails
-                = new ErrorResponseBean(new Date(), HttpStatus.NOT_MODIFIED.toString(), ex.getMessage(), request.getDescription(false));
+                = new ErrorResponseBean(new Date(), HttpStatus.NOT_MODIFIED.name(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_MODIFIED);
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<?> ValidationException(ValidationException ex, WebRequest request) {
         ErrorResponseBean errorDetails
-                = new ErrorResponseBean(new Date(), HttpStatus.NOT_ACCEPTABLE.toString(), ex.getMessage(), request.getDescription(false));
+                = new ErrorResponseBean(new Date(), HttpStatus.NOT_ACCEPTABLE.name(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_ACCEPTABLE);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> AppExceptionHandler(Exception ex, WebRequest request) {
         ErrorResponseBean errorDetails
-                = new ErrorResponseBean(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.toString(), ex.getMessage(), request.getDescription(false));
+                = new ErrorResponseBean(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.name(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
