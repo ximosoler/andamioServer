5e6a48277eb8941da3e2e19821d1661d952bddf3
rafaelaznar <rafaaznar@gmail.com>
2022-06-08 20:49:31 +0200

    refactorización API y servicios

--- src/main/java/net/ausiasmarch/wildcart/service/CompraService.java
+++ src/main/java/net/ausiasmarch/wildcart/service/CompraService.java
@@ -7,10 +7,23 @@ import java.util.concurrent.ThreadLocalRandom;
 import net.ausiasmarch.wildcart.entity.CompraEntity;
 import net.ausiasmarch.wildcart.entity.FacturaEntity;
 import net.ausiasmarch.wildcart.entity.ProductoEntity;
+import net.ausiasmarch.wildcart.entity.UsuarioEntity;
+import net.ausiasmarch.wildcart.exception.ResourceNotFoundException;
+import net.ausiasmarch.wildcart.helper.ValidationHelper;
+import net.ausiasmarch.wildcart.repository.CompraRepository;
 import net.ausiasmarch.wildcart.repository.FacturaRepository;
 import net.ausiasmarch.wildcart.repository.ProductoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
 public class CompraService {
@@ -23,10 +36,94 @@ public class CompraService {
 
     @Autowired
     FacturaService oFacturaService;
+    @Autowired
+    UsuarioService oUsuarioService;
 
     @Autowired
     ProductoService oProductoService;
-       
+
+    @Autowired
+    AuthService oAuthService;
+
+    @Autowired
+    CompraRepository oCompraRepository;
+
+    public void validate(Long id) {
+        if (!oCompraRepository.existsById(id)) {
+            throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
+
+    public void validate(CompraEntity oCompraEntity) {
+        ValidationHelper.validateRange(oCompraEntity.getCantidad(), 1, 1000, "campo cantidad de la entidad compra (debe ser un entero entre 1 y 1000)");
+        //el precio sale de la bd: se copia del precio del producto, se supone que está validado. Lo mismo los descuentos. La fecha se toma del servidor.
+        ValidationHelper.validateRange(oCompraEntity.getPrecio(), 1, 1000, "campo cantidad de la entidad compra (debe ser un entero entre 0 y 1000000)");
+        ValidationHelper.validateRange(oCompraEntity.getDescuento_producto(), 0, 100, "campo descuento de producto de la entidad compra (debe ser un entero entre 0 y 100)");
+        ValidationHelper.validateRange(oCompraEntity.getDescuento_usuario(), 0, 100, "campo descuento de usuario de la entidad compra (debe ser un entero entre 0 y 100)");
+        ValidationHelper.validateDate(oCompraEntity.getFecha(), LocalDateTime.of(1990, 01, 01, 00, 00, 00), LocalDateTime.of(2025, 01, 01, 00, 00, 00), "campo fecha de compra");
+    }
+
+    public CompraEntity get(Long id) {
+        validate(id);
+        oFacturaService.validate(oCompraRepository.getById(id).getFactura().getId());
+        oUsuarioService.validate(oCompraRepository.getById(id).getFactura().getUsuario().getId());
+        oAuthService.OnlyAdminsOrOwnUsersData(oCompraRepository.getById(id).getFactura().getUsuario().getId()); //no pueden haber compras sin facturar en la BD!!
+        return oCompraRepository.getById(id);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Long> count() {
+        oAuthService.OnlyAdmins();
+        return new ResponseEntity<Long>(oCompraRepository.count(), HttpStatus.OK);
+    }
+
+    public Page<CompraEntity> getPage(Pageable oPageable, String strFilter, Long lFactura, Long lProducto) {
+        Page<CompraEntity> oPage = null;
+        if (oAuthService.isAdmin()) {
+            if (lFactura != null) {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByFacturaIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProducto(lFactura, strFilter, strFilter, strFilter, strFilter, strFilter, oPageable);
+                } else {
+                    oPage = oCompraRepository.findByFacturaId(lFactura, oPageable);
+                }
+            } else if (lProducto != null) {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByProductoIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProducto(lProducto, strFilter, strFilter, strFilter, strFilter, strFilter, oPageable);
+                } else {
+                    oPage = oCompraRepository.findByProductoId(lProducto, oPageable);
+                }
+            } else {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByIdContain(strFilter, strFilter, strFilter, strFilter, strFilter, strFilter, oPageable);
+                } else {
+                    oPage = oCompraRepository.findAll(oPageable);
+                }
+            }
+        } else {
+            if (lFactura != null) {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByFacturaIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProductoUsuario(lFactura, strFilter, strFilter, strFilter, strFilter, strFilter, oAuthService.getUserID(), oPageable);
+                } else {
+                    oPage = oCompraRepository.findByFacturaIdUsuario(lFactura, oAuthService.getUserID(), oPageable);
+                }
+            } else if (lProducto != null) {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByProductoIdAndCantidadOrPrecioOrFechaOrDescuentoUsuarioOrDescuentoProductoUsuario(lProducto, strFilter, strFilter, strFilter, strFilter, strFilter, oAuthService.getUserID(), oPageable);
+                } else {
+                    oPage = oCompraRepository.findByProductoIdUsuario(lProducto, oAuthService.getUserID(), oPageable);
+                }
+            } else {
+                if (strFilter != null) {
+                    oPage = oCompraRepository.findByIdContainUsuario(strFilter, strFilter, strFilter, strFilter, strFilter, strFilter, oAuthService.getUserID(), oPageable);
+                } else {
+                    oPage = oCompraRepository.findAllUsuario(oAuthService.getUserID(), oPageable);
+                }
+            }
+
+        }
+        return oPage;
+    }
+
     public CompraEntity generateRandomCompra() {
         CompraEntity oCompraEntity = new CompraEntity();
         oCompraEntity.setCantidad(generateCantidad(1, 200));
@@ -63,9 +160,6 @@ public class CompraService {
     private int generateDescuentoProducto(int minValue, int maxValue) {
         return ThreadLocalRandom.current().nextInt(minValue, maxValue);
     }
-
-
-    
 
     private FacturaEntity generateFactura() {
         List<FacturaEntity> list = oFacturaRepository.findAll();
