b066069c3891c9ba2c04065921f13c19aedcb536
rafaelaznar <rafaaznar@gmail.com>
2022-09-05 08:54:15 +0200

    new view -> preparing project to show carrito per product in home when user is authenticated

--- src/main/java/net/ausiasmarch/wildcart/api/ProductoCarritoViewController.java
+++ src/main/java/net/ausiasmarch/wildcart/api/ProductoCarritoViewController.java
@@ -49,7 +49,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/productocarrito")
+@RequestMapping("/productocarritoview")
 public class ProductoCarritoViewController {
 
     @Autowired
