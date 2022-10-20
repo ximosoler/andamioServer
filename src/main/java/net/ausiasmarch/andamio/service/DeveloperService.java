77a3fcbce5473f8d8e093f38a29e218c6af053db
alvarotalaya <altaro2002@gmail.com>
2022-10-20 20:37:32 +0200

    added developer service

---
+++ src/main/java/net/ausiasmarch/andamio/service/DeveloperService.java
@@ -0,0 +1,19 @@
+package net.ausiasmarch.andamio.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.transaction.Transactional;
+import net.ausiasmarch.andamio.exception.UnauthorizedException;
+import net.ausiasmarch.andamio.exception.CannotPerformOperationException;
+import net.ausiasmarch.andamio.exception.ResourceNotFoundException;
+import net.ausiasmarch.andamio.helper.RandomHelper;
+import net.ausiasmarch.andamio.helper.ValidationHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@Service
+public class DeveloperService {
+
+}
