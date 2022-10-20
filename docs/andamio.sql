47c110a819d6e00e92e3b1d4b44e60122318a97f
RAFAEL AZNAR <rafaaznar@gmail.com>
2022-10-20 17:32:31 +0200

    fix database script

--- docs/andamio.sql
+++ docs/andamio.sql
@@ -1,45 +1,19 @@
-create database andamio;
-
-use andamio;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
--- Estructura de tabla para la tabla `project`
---
-CREATE TABLE `project` (
-`id` bigint auto_increment NOT NULL,
-`project_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`project_description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-`url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-PRIMARY KEY (id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- Servidor: localhost:3306
+-- Tiempo de generación: 20-10-2022 a las 15:30:15
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
--- ---------------------------------
-
--- 
--- Estructura de tabla para la tabla `help`
--- 
-
-CREATE TABLE `help`(
-	`id` bigint NOT NULL AUTO_INCREMENT,
-    `id_resolution` bigint NOT NULL,
-    `id_developer` bigint NOT NULL,
-    `percentage` double NOT NULL,
-    PRIMARY KEY(id)
-)ENGINE = InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- ---------------------------------
-
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 --
--- Estructura de tabla para la tabla `task`
+-- Base de datos: `andamio`
 --
-
-CREATE TABLE `task` (
-  `id` bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `id_project` bigint NOT NULL,
-  `priority` int NOT NULL DEFAULT '0',
-  `complexity` int NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -47,64 +21,172 @@ CREATE TABLE `task` (
 -- Estructura de tabla para la tabla `developer`
 --
 
-
 CREATE TABLE `developer` (
-    `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `surname` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `last_name` VARCHAR(255)CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-    `administrador` tinyint(1) NOT NULL
-    PRIMARY KEY(id)
-    )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `surname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `administrador` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-    -- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `developer`
+--
 
-INSERT INTO project (id,project_code, project_description, url) VALUES
-(1,"aabb","Example1","https://example1/andamios.net"),
-(2,"aaabbb","Example2","https://example2/andamios.net"),
-(3,"ccdd","Example3","https://example3/andamios.net"),
-(4,"cccddd","Example4","https://example4/andamios.net"),
-(5,"eeff","Example5","https://example5/andamios.net"),
-(6,"eeefff","Example6","https://example6/andamios.net"),
-(7,"gghh","Example7","https://example7/andamios.net"),
-(8,"ggghhh","Example8","https://example8/andamios.net"),
-(9,"iijj","Example9","https://example9/andamios.net"),
-(10,"iiijjj","Example10","https://example10/andamios.net");
+INSERT INTO `developer` (`id`, `name`, `surname`, `last_name`, `email`, `administrador`) VALUES
+(1, 'raimon', 'vilar', 'morera', 'test@email.com', 0),
+(2, 'alvaro', 'talaya', 'romance', 'test@email.com', 0),
+(3, 'mario', 'tomas', 'zanon', 'test@email.com', 0),
+(4, 'aitana', 'collado', 'soler', 'test@email.com', 0),
+(5, 'carlos', 'merlos', 'pilar', 'test@email.com', 0),
+(6, 'luis', 'perez', 'derecho', 'test@email.com', 0),
+(7, 'estefania', 'boriko', 'izquierdo', 'test@email.com', 0),
+(8, 'quique', 'aroca', 'garcia', 'test@email.com', 0),
+(9, 'adrian', 'duyang', 'liang', 'test@email.com', 0),
+(10, 'rafael', 'aznar', 'caballero', 'test@email.com', 1);
 
-INSERT INTO `help` (`id`, `id_resolution`, `id_developer`, `percentage`) VALUES 
-(1, 1, 1, 10), 
-(2, 5, 8, 8), 
-(3, 3, 9, 0.1), 
-(4, 9, 2, 0.5), 
-(5, 7, 5, 5), 
-(6, 6, 8, 15), 
-(7, 2, 6, 12), 
-(8, 8, 2, 3), 
-(9, 4, 7, 1), 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `help`
+--
+
+CREATE TABLE `help` (
+  `id` bigint(20) NOT NULL,
+  `id_resolution` bigint(20) NOT NULL,
+  `id_developer` bigint(20) NOT NULL,
+  `percentage` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `help`
+--
+
+INSERT INTO `help` (`id`, `id_resolution`, `id_developer`, `percentage`) VALUES
+(1, 1, 1, 10),
+(2, 5, 8, 8),
+(3, 3, 9, 0.1),
+(4, 9, 2, 0.5),
+(5, 7, 5, 5),
+(6, 6, 8, 15),
+(7, 2, 6, 12),
+(8, 8, 2, 3),
+(9, 4, 7, 1),
 (10, 10, 4, 10);
 
+-- --------------------------------------------------------
 
-INSERT INTO `task` VALUES
-(1, 'SQL db test',2,4,8),
-(2, 'Inno db is cool',4,3,9),
-(3, 'administrador SQL test',2,4,8),
-(4, 'MongoDB',2,4,8),
-(5, 'Hola mundo!',4,4,8),
-(6, 'Adios Mundo!',6,3,8),
-(7, 'Say hello!',9,5,8),
-(8, 'My cat bigotillos',2,4,8),
-(9, 'The mexican',9,9,8),
-(10, 'Another one',1,1,1);
+--
+-- Estructura de tabla para la tabla `project`
+--
 
-INSERT INTO `developer` (`name`, `surname`, `second_name`, `email`, `administrador`) VALUES 
-('raimon', 'vilar', 'morera', 'test@email.com',  0), 
-('alvaro', 'talaya', 'romance', 'test@email.com', 0), 
-('mario', 'tomas', 'zanon', 'test@email.com', 0),
-('aitana', 'collado', 'soler', 'test@email.com', 0), 
-('carlos', 'merlos', 'pilar', 'test@email.com', 0), 
-('luis', 'perez', 'derecho', 'test@email.com', 0), 
-('estefania', 'boriko', 'izquierdo', 'test@email.com', 0),
-('quique', 'aroca', 'garcia', 'test@email.com', 0), 
-('adrian', 'duyang', 'liang', 'test@email.com', 0), 
-('rafael', 'aznar', 'caballero', 'test@email.com', 1);
+CREATE TABLE `project` (
+  `id` bigint(20) NOT NULL,
+  `project_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `project_description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `project`
+--
+
+INSERT INTO `project` (`id`, `project_code`, `project_description`, `url`) VALUES
+(1, 'aabb', 'Example1', 'https://example1/andamios.net'),
+(2, 'aaabbb', 'Example2', 'https://example2/andamios.net'),
+(3, 'ccdd', 'Example3', 'https://example3/andamios.net'),
+(4, 'cccddd', 'Example4', 'https://example4/andamios.net'),
+(5, 'eeff', 'Example5', 'https://example5/andamios.net'),
+(6, 'eeefff', 'Example6', 'https://example6/andamios.net'),
+(7, 'gghh', 'Example7', 'https://example7/andamios.net'),
+(8, 'ggghhh', 'Example8', 'https://example8/andamios.net'),
+(9, 'iijj', 'Example9', 'https://example9/andamios.net'),
+(10, 'iiijjj', 'Example10', 'https://example10/andamios.net');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `task`
+--
+
+CREATE TABLE `task` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id_project` bigint(20) NOT NULL,
+  `priority` int(11) NOT NULL DEFAULT 0,
+  `complexity` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `task`
+--
+
+INSERT INTO `task` (`id`, `description`, `id_project`, `priority`, `complexity`) VALUES
+(1, 'SQL db test', 2, 4, 8),
+(2, 'Inno db is cool', 4, 3, 9),
+(3, 'administrador SQL test', 2, 4, 8),
+(4, 'MongoDB', 2, 4, 8),
+(5, 'Hola mundo!', 4, 4, 8),
+(6, 'Adios Mundo!', 6, 3, 8),
+(7, 'Say hello!', 9, 5, 8),
+(8, 'My cat bigotillos', 2, 4, 8),
+(9, 'The mexican', 9, 9, 8),
+(10, 'Another one', 1, 1, 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `developer`
+--
+ALTER TABLE `developer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `help`
+--
+ALTER TABLE `help`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `developer`
+--
+ALTER TABLE `developer`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `help`
+--
+ALTER TABLE `help`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
