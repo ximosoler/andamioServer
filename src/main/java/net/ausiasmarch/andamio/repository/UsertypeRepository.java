b10e33ae77b9d7c1a89ec7d279dd40064140c9e1
helmets7 <nereamarisa56@gmail.com>
2022-10-20 20:13:33 +0200

    Help and usertype repository finished

---
+++ src/main/java/net/ausiasmarch/andamio/repository/UsertypeRepository.java
@@ -0,0 +1,8 @@
+package net.ausiasmarch.andamio.repository;
+
+import net.ausiasmarch.andamio.entity.UsertypeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsertypeRepository extends JpaRepository<UsertypeEntity, Long> {
+
+}
