47ffc8632deb479a5ea6d043623f9ca026d05df0
alvarotalaya <altaro2002@gmail.com>
2022-10-20 21:29:16 +0200

    added developer controller

--- src/main/java/net/ausiasmarch/andamio/service/DeveloperService.java
+++ src/main/java/net/ausiasmarch/andamio/service/DeveloperService.java
@@ -1,19 +1,23 @@
 package net.ausiasmarch.andamio.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import javax.transaction.Transactional;
-import net.ausiasmarch.andamio.exception.UnauthorizedException;
-import net.ausiasmarch.andamio.exception.CannotPerformOperationException;
-import net.ausiasmarch.andamio.exception.ResourceNotFoundException;
-import net.ausiasmarch.andamio.helper.RandomHelper;
-import net.ausiasmarch.andamio.helper.ValidationHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
+import net.ausiasmarch.andamio.entity.DeveloperEntity;
+import net.ausiasmarch.andamio.exception.ResourceNotFoundException;
+import net.ausiasmarch.andamio.repository.DeveloperRepository;
 
 @Service
 public class DeveloperService {
+    @Autowired
+    DeveloperRepository oDeveloperRepository;
 
+    public DeveloperEntity get(Long id) {
+        try {
+            return oDeveloperRepository.findById(id).get();
+        } catch (Exception ex) {
+            throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
 }
+
