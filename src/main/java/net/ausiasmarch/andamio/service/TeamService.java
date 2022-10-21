d514d78eca99aa3779ebba146b752bf83140bd12
KevinBB <blancodaw22@gmail.com>
2022-10-21 17:37:26 +0200

    Añadidos metodos Get (Project y Team)

--- src/main/java/net/ausiasmarch/andamio/service/TeamService.java
+++ src/main/java/net/ausiasmarch/andamio/service/TeamService.java
@@ -1,8 +1,26 @@
 package net.ausiasmarch.andamio.service;
 
+
+import net.ausiasmarch.andamio.entity.TeamEntity;
+import net.ausiasmarch.andamio.repository.TeamRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
     
+    private final TeamRepository oTeamRepository;
+
+    @Autowired
+    public TeamService(TeamRepository oTeamRepository) {
+        this.oTeamRepository = oTeamRepository;
+    }
+
+    public TeamEntity get(Long id) {
+        return oTeamRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Team with id: " + id + " not found"));
+
+    }
+
 }
