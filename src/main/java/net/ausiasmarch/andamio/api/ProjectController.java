2e1ff8010dd2b8b8838479cdcda5c344de30db50
Raimon Vilar <raimonv@gmail.com>
2022-10-25 20:48:55 +0200

    METODO UPDATE DE PROJECT

--- src/main/java/net/ausiasmarch/andamio/api/ProjectController.java
+++ src/main/java/net/ausiasmarch/andamio/api/ProjectController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +32,11 @@ public class ProjectController {
     @GetMapping("/count")
     public ResponseEntity<Long> count() {
         return new ResponseEntity<Long>(oProjectService.count(), HttpStatus.OK);
+    }
+    
+    @PutMapping("")
+    public ResponseEntity<Long> update(@RequestBody ProjectEntity oProjectEntity) {
+        return new ResponseEntity<Long>(oProjectService.update(oProjectEntity), HttpStatus.OK);
     }
     
 }
