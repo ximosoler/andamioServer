23563f44a6329d1ddf3aa419d14a7f1e19e7d649
Luis Perez Hurtado <lperezhurtado97@gmail.com>
2022-10-27 21:59:17 +0200

    Añadido delete de issue

--- src/main/java/net/ausiasmarch/andamio/service/IssueService.java
+++ src/main/java/net/ausiasmarch/andamio/service/IssueService.java
@@ -1,6 +1,7 @@
 package net.ausiasmarch.andamio.service;
 
 import net.ausiasmarch.andamio.entity.IssueEntity;
+import net.ausiasmarch.andamio.exception.ResourceNotFoundException;
 import net.ausiasmarch.andamio.repository.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,17 @@ public class IssueService {
         oAuthService.OnlyAdmins();
         return oIssueRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Issue with id: " + id + " not found"));
+    }
+
+    public void validate(Long id) {
+        if (!oIssueRepository.existsById(id)) {
+            throw new ResourceNotFoundException("id " + id + " not exist");
+        }
+    }
+    public Long delete(Long id) {
+        oAuthService.OnlyAdmins();
+        validate(id);
+        oIssueRepository.deleteById(id);
+        return id;
     }
 }
