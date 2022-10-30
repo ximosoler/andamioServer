533c277e0e67b3f94da781e99c703b4dbffcedee
ElliotLuque <elliotluque@gmail.com>
2022-10-30 11:44:49 +0100

    added help delete

--- src/main/java/net/ausiasmarch/andamio/api/HelpController.java
+++ src/main/java/net/ausiasmarch/andamio/api/HelpController.java
@@ -3,6 +3,7 @@ package net.ausiasmarch.andamio.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,10 @@ public class HelpController {
     @GetMapping("/{id}")
     public ResponseEntity<HelpEntity> get(@PathVariable(value = "id") Long id) {
         return new ResponseEntity<HelpEntity>(oHelpService.get(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long delete(@PathVariable(value = "id") Long id){
+        return oHelpService.delete(id);
     }
 }
