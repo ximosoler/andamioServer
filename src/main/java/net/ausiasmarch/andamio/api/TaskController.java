e03fde43c56165b699608b1c98510c0931602592
Esborlan <esborlan.daw@gmail.com>
2022-11-02 23:33:31 +0100

    Añadido getPage a la entidad Task

--- src/main/java/net/ausiasmarch/andamio/api/TaskController.java
+++ src/main/java/net/ausiasmarch/andamio/api/TaskController.java
@@ -1,6 +1,9 @@
 package net.ausiasmarch.andamio.api;
 
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 
 import net.ausiasmarch.andamio.entity.TaskEntity;
 import net.ausiasmarch.andamio.service.TaskService;
@@ -54,5 +60,13 @@ public class TaskController {
     @PostMapping("/generate/{amount}")
     public ResponseEntity<Long> generateSome(@PathVariable(value = "amount") Integer amount) {
         return new ResponseEntity<>(oTaskService.generateSome(amount), HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Page<TaskEntity>> getPage(
+            @ParameterObject @PageableDefault(page = 0, size = 10, direction = Sort.Direction.DESC) Pageable oPageable,
+            @RequestParam(name = "filter", required = false) String strFilter,
+            @RequestParam(name = "project", required = false) Long lProject) {
+        return new ResponseEntity<Page<TaskEntity>>(oTaskService.getPage(oPageable, strFilter, lProject), HttpStatus.OK);
     }
 }
