2f3fb2b94cb1bb1a633ce20fcf1de0542679e941
cristinacarrascosa <cctausias@gmail.com>
2022-11-02 12:47:31 +0100

    hecho generateSome de project

--- src/main/java/net/ausiasmarch/andamio/api/ProjectController.java
+++ src/main/java/net/ausiasmarch/andamio/api/ProjectController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +59,16 @@ public class ProjectController {
     @DeleteMapping("/{id}")
     public Long delete(@PathVariable(value = "id") Long id){
         return oProjectService.delete(id);
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<ProjectEntity> generate() {
+        return new ResponseEntity<ProjectEntity>(oProjectService.generate(), HttpStatus.OK);
+    }
+    
+    @PostMapping("/generate/{amount}")
+    public ResponseEntity<Long> generateSome(@PathVariable(value = "amount") Integer amount) {
+        return new ResponseEntity<>(oProjectService.generateSome(amount), HttpStatus.OK);
     }
     
 }
